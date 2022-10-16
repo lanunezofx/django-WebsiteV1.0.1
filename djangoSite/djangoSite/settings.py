8651d14e68b8d68541a1b6bc4e13c687aeeafb57
LA Nuñez <lanunez.ofx@gmail.com>
2022-10-16 23:21:46 +0800

    SEC-001
    Commit <10.16.22>
    - Sec-001 - DONE
    - Decouple - DONE
    - Impt Config - DONE

--- djangoSite/djangoSite/settings.py
+++ djangoSite/djangoSite/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qw%&%@eo5u_cv+(+8p$u7l6kr%_0r0y1x)@a1oe#)v^x8+haum'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
